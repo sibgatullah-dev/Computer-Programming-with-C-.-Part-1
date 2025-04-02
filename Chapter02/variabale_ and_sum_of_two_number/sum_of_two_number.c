6e88f1f0544432b26e0e189ed7c8aa2fe8ed7d97
Sibgatullah <sibgatullah881@gmail.com>
2025-04-03 00:33:04 +0600

    chapter2

---
+++ Chapter02/variabale_ and_sum_of_two_number/sum_of_two_number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum ;
+    
+
+    a=50;
+    b=60;
+
+    sum = a+b;
+
+    printf("Sum is %d",sum);// %d is used to print integer values, here we said that sum is integer and will be printed in %d. 
+
+    return 0;
+}
+
